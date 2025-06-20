dac3abf3983f14b36e35ae981d42633561f808f9
Haridugrani <bhargavdungrani32@gmail.com>
2025-06-20 13:37:55 +0530

    some changes

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const userRoutes = require('./routes/userRoutes');
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(express.json());
-// app.use('/users', userRoutes);
-
-// app.use((req, res) => {
-//     res.status(404).json({ error: 'Not Found' });
-// });
-
-// app.get('/', (req, res) => {
-//     res.send('Server is running');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`✅ Server running at http://localhost:${PORT}`);
-// });
-
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
